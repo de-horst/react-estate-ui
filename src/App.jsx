28925bf7d80c2e8a5aab9789ed7c4cff484f186a
Denise Horst <denisef.horst@gmail.com>
2024-03-07 21:36:38 -0300

    03 List Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
 
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// TODO: 1:04:19
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     const [open, setOpen] = useState(false)
@@ -7,32 +8,32 @@ function Navbar() {
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to={"/"} className="logo">
           <img src="/logo.png" />
           <span>LamaEstate</span>
-        </a>
+        </Link>
 
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>About</Link>
+        <Link to={"/"}>Contact</Link>
+        <Link to={"/"}>Agents</Link>
       </div>
 
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">Sign up</a>
+        <Link to={"/"}>Sign in</Link>
+        <Link to={"/"} className="register">Sign up</Link>
 
         <div className="menuIcon">
             <img src="/menu.png" alt="" onClick={()=>setOpen((prev) => !prev)} />
         </div>
 
         <div className={open ? "menu active" : "menu"}>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
-        <a href="/">Sign in</a>
-        <a href="/">Sign up</a>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>About</Link>
+        <Link to={"/"}>Contact</Link>
+        <Link to={"/"}>Agents</Link>
+        <Link to={"/"}>Sign in</Link>
+        <Link to={"/"}>Sign up</Link>
         </div>
       </div>
     </nav>
