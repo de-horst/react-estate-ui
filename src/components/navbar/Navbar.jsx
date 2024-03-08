2c21c75cbcc8e4b587f559c293a48d0ee7f1b0bf
Denise Horst <denisef.horst@gmail.com>
2024-03-08 20:50:07 -0300

    05 Profile Page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,9 @@ import "./navbar.scss";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+
+  const user = true;
 
   return (
     <nav>
@@ -20,20 +22,42 @@ function Navbar() {
       </div>
 
       <div className="right">
-        <Link to={"/"}>Sign in</Link>
-        <Link to={"/"} className="register">Sign up</Link>
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link to={"/"}>Sign in</Link>
+            <Link to={"/"} className="register">
+              Sign up
+            </Link>
+          </>
+        )}
 
         <div className="menuIcon">
-            <img src="/menu.png" alt="" onClick={()=>setOpen((prev) => !prev)} />
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
 
         <div className={open ? "menu active" : "menu"}>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/"}>About</Link>
-        <Link to={"/"}>Contact</Link>
-        <Link to={"/"}>Agents</Link>
-        <Link to={"/"}>Sign in</Link>
-        <Link to={"/"}>Sign up</Link>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>About</Link>
+          <Link to={"/"}>Contact</Link>
+          <Link to={"/"}>Agents</Link>
+          <Link to={"/"}>Sign in</Link>
+          <Link to={"/"}>Sign up</Link>
         </div>
       </div>
     </nav>
